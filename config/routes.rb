ff6e280741afd63c5675f78a90eb2c862b2ed267
Dyan Carra <dyancarra@Dyans-MacBook-Air.local>
2024-08-24 09:28:12 -0300

    Add devise with User model

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  devise_for :users
+
   mount Sidekiq::Web => "/sidekiq"
 end
